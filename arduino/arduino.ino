4cadc4f8d2f42631ec5d7e5b3d41ae974d2c0992
nvarcha <nvarcha@gmail.com>
2020-03-21 16:29:53 -0300

    Update arduino.ino
    More updates

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,21 +1,83 @@
 /**
  * Respirator project - GNU
  * 
- * This is free and delivered AS IS. Use at your own risk.
+ * Copyright (c) 2020 - Nicolas Varchavsky, Joel Ramos Pacheco, Sebastian Bourre - All rights reserved
  */
 
 // Set to 1 to enable serial debug, 0 to disable it
 #define DEBUG 1
+#define OPEN true
+#define CLOSED false
+
+/********************************/
+/************ PINS **************/
+/********************************/
+ 
+/**
+ * Pressure sensors pin numbers
+ * These are read through I2C
+ */
+const int ps1_o2_in = 0;
+const int ps2_air_in = 0;
+const int ps3_mixed_gas_in = 0;
+const int ps4_input_gas_in = 0;
+
+/**
+ * Eletro-valves pin numbers
+ * These are activated through relay boards
+ */
+const int ev1_o2_in = 11;
+const int ev2_air_in = 12;
+const int ev3_mixed_gas_out = 13;
+const int ev4_input_gas = 14;
+const int ev5_output_gas = 15;
+
+/**
+ * Electro-valves status
+ */
+bool ev1_status = CLOSED;
+
+/**
+ * Flow-control flowmeter or rotameter.
+ * These are to control and sense flow and handled through I2C
+ * TODO: I'm temporarily using pure analog until I can get something
+ * that has I2C compatibility
+ */
+const int fc1_o2 = A5;
+const int fc2_air = A4;
+const int fc3_gas = A3;
+
+/**
+ * Flow-control read values
+ * Stored mostly for debugging and feedback purposes
+ */
+int fc1_value = 0;
+
+/**
+ * Flow-control thresholds
+ * These thresholds identified values that, when greater or equal, are 
+ * considered a positive flow
+ */
+ const int fc1_active_threshold = 400;
+
+/**
+ * Possible pressure sensor used to sense if the patient is breathing
+ * on her own
+ */
+const int s1_breathing = 0;
+
+ /**
+  * Actuator servo or stepper motor to inflate and deflate the AMBU bag
+  */
+const int act1_ambu = 0;
+
+/*********************************************/
+/************ GENERAL VARIABLES **************/
+/*********************************************/
 
 // Start/stop (probably temporary)
 const int pin_start_stop_button = 2;
 volatile bool poweredOn = false;
-
-// O2 valve relay control pin
-const int pin_o2_in_valve = 11;
-
-// O2 valve opened (false = closed, true = open)
-bool o2_in_valve_opened = false;
 
 /**
  * Initial setup code
@@ -35,7 +97,7 @@ void setup() {
   #endif
   
   // Configure the following pins as output pins
-  pinMode(pin_o2_in_valve, OUTPUT);
+  pinMode(ev1_o2_in, OUTPUT);
 
   // Configure the following pins as input pins
   // INPUT_PULLUP inverts the behaviour so HIGH means off and LOW means on
@@ -63,15 +125,20 @@ void togglePower() {
  */
 void loop() {
 
+  fc1Read();
+  Serial.println("FC1 value");
+  Serial.println(fc1_value);
+  Serial.println("FC1 status");
+  Serial.println(fc1Positive());
+
   // Early out if we're powered off
   if (!poweredOn) {
     return;
   }
-  
-  
-  digitalWrite(pin_o2_in_valve,HIGH);
+
+  ev1Open();
   delay(1000);
-  digitalWrite(pin_o2_in_valve,LOW);
+  ev1Close();
   delay(1000);
 }
 
@@ -84,4 +151,52 @@ void startupDiagnostics() {
   #ifdef DEBUG
   Serial.println("Starting diagnostics");
   #endif
+
+  // TEST O2 intake
+  // Open O2 valve
+  // Check O2 flowmeter
+  // Are we getting a positive flow?
+  // If not, alert (sound, email, whatever)
+  // Close O2 valve
+
+  // TEST Air intake
+  // Open Air valve
+  // Check Air flowmeter
+  // Are we getting a positive flow?
+  // If not, alert (sound, email, whatever)
+  // Close Air valve
+
+  
+  
+}
+
+/*********************************/
+/**** Electro vale operations ****/
+/*********************************/
+void ev1Open() {
+  digitalWrite(ev1_o2_in,HIGH);
+}
+
+void ev1Close() {
+  digitalWrite(ev1_o2_in,LOW);
+}
+
+
+/*********************************/
+/**** Flow control operations ****/
+/*********************************/
+
+/**
+ * Read the FC1 value
+ */
+void fc1Read() {
+  fc1_value = analogRead(fc1_o2);
+}
+
+/**
+ * Returns true when the value of the FC1 is
+ * equal or greater than the specified threshold
+ */
+bool fc1Positive() {
+  return (fc1_value >= fc1_active_threshold);
 }
